0085dcab4e7ae35560b2f411288d52629d94b275
Christiana Ukoli <cukoli22@gmail.com>
2022-06-27 18:42:40 -0500

    adding remaining states

--- server.js
+++ server.js
@@ -138,6 +138,81 @@ const states = {
         "abbreviation": "MT",
         "cap population": 32000
     },
+    "nebraska": {
+        "capital": "Lincoln",
+        "abbreviation": "NE",
+        "cap population": 287000
+    },
+    "nevada": {
+        "capital": "Carson City",
+        "abbreviation": "NV",
+        "cap population": 55000
+    },
+    "new hampshire": {
+        "capital": "Concord",
+        "abbreviation": "NH",
+        "cap population": 43000
+    },
+    "new jersey": {
+        "capital": "Trenton",
+        "abbreviation": "NJ",
+        "cap population": 83000
+    },
+    "new mexico": {
+        "capital": "Santa Fe",
+        "abbreviation": "NM",
+        "cap population": 84000
+    },
+    "new york": {
+        "capital": "Albany",
+        "abbreviation": "NY",
+        "cap population": 97000
+    },
+    "north carolina": {
+        "capital": "Raleigh",
+        "abbreviation": "NC",
+        "cap population": 469000
+    },
+    "north dakota": {
+        "capital": "Bismarck",
+        "abbreviation": "ND",
+        "cap population": 73000
+    },
+    "ohio": {
+        "capital": "Columbus",
+        "abbreviation": "OH",
+        "cap population": 892000
+    },
+    "oklahoma": {
+        "capital": "Oklahoma City",
+        "abbreviation": "OK",
+        "cap population": 649000
+    },
+    "oregon": {
+        "capital": "Salem",
+        "abbreviation": "OR",
+        "cap population": 173000
+    },
+    "pennsylvania": {
+        "capital": "Harrisburg",
+        "abbreviation": "PA",
+        "cap population": 49000
+    },
+    "rhode island": {
+        "capital": "Providence",
+        "abbreviation": "RI",
+        "cap population": 179000
+    },
+    "south carolina": {
+        "capital": "Columbia",
+        "abbreviation": "SC",
+        "cap population": 133000
+    },
+    "south dakota": {
+        "capital": "Pierre",
+        "abbreviation": "SD",
+        "cap population": 13000
+    },
     "tennessee": {
         "capital": "Nashville",
         "abbreviation": "TN",
@@ -147,6 +222,41 @@ const states = {
         "capital": "Austin",
         "abbreviation": "TX",
         "cap population": 954000
+    },
+    "utah": {
+        "capital": "Salt Lake City",
+        "abbreviation": "UT",
+        "cap population": 200000
+    },
+    "vermont": {
+        "capital": "Montpelier",
+        "abbreviation": "VT",
+        "cap population": 7000
+    },
+    "virginia": {
+        "capital": "Richmond",
+        "abbreviation": "VA",
+        "cap population": 228000
+    },
+    "washington": {
+        "capital": "Olympia",
+        "abbreviation": "WA",
+        "cap population": 52000
+    },
+    "west virginia": {
+        "capital": "Charleston",
+        "abbreviation": "WV",
+        "cap population": 47000
+    },
+    "wisconsin": {
+        "capital": "Madison",
+        "abbreviation": "WI",
+        "cap population": 255000
+    },
+    "wyoming": {
+        "capital": "Cheyenne",
+        "abbreviation": "WY",
+        "cap population": 63000
     },
     "unknown":{
         "capital": "unknown",
